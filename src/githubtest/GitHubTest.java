37df115bb896722813e70a04f6adf3567b2e367b
jake <jake@DESKTOP-K42F3OR.lan>
2026-01-21 21:15:03 -0600

    This adds an example branch in which we make a sample script that verify the Graphics2D and Swing components work properly.

--- src/githubtest/GitHubTest.java
+++ src/githubtest/GitHubTest.java
@@ -4,18 +4,33 @@
  */
 package githubtest;
 
-/**
- *
- * @author jake
- */
-public class GitHubTest {
+import javax.swing.*;
+import java.awt.*;
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("Hello Github!");
+public class GitHubTest extends JPanel {
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2d = (Graphics2D) g;
+        
+        g2d.setColor(Color.BLUE);
+        g2d.fillRect(50, 50, 100, 100);
+        
+        g2d.setColor(Color.BLACK);
+        g2d.drawString("Java 21 Setup Verified!", 50, 40);
+        
+        String version = System.getProperty("java.version");
+        g2d.drawString("Running on JDK: " + version, 50, 180);
     }
-    
-}
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("CS321 Setup Test");
+        // FIX: Changed from SetupTest to GitHubTest
+        frame.add(new GitHubTest()); 
+        frame.setSize(300, 300);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    } // Added missing brace for main method
+} // Added missing brace for class
