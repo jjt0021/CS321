35daaf579946076929fa9d24dc8576fbe151a0ed
Elijah Moore <elijahmoore2019@gmail.com>
2026-03-04 10:49:52 -0600

    Split the prefetchAndCleanUP() function to make more readable

--- src/main/java/com/mycompany/pdftest/playState.java
+++ src/main/java/com/mycompany/pdftest/playState.java
@@ -3,12 +3,10 @@ package com.mycompany.pdftest;
 import java.io.File;
 import java.util.ArrayList;
 import com.mycompany.pdftest.Audio;
+import com.mycompany.pdftest.Settings.TTSmodel;
 import java.util.HashMap;
 import java.util.Map;
 
-import com.mycompany.pdftest.Settings;
-import com.mycompany.pdftest.Settings.SettingsValues;
-import com.mycompany.pdftest.Settings.TTSmodel;
 
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
@@ -21,10 +19,6 @@ import com.mycompany.pdftest.Settings.TTSmodel;
 //This classes job keeps track of the current chunk, checks if a reload is needed for the gui, and keeps track of if the playback is true
 public class playState {
 
-    Settings initialSettingObj;
-    SettingsValues initialSettings = initialSettingObj.getSettingsValues();
-    TTSmodel initialModle;
-
     private int currentChunk;
 
     private boolean isPlaying;
@@ -34,70 +28,80 @@ public class playState {
 
     private int loadedRange = 100;
     private int reloadRange = 30;
-    Map<Integer, Audio> cachedWindow = new HashMap<>();
+    int cacheSize = 5;
+    private TTSmodel initalModel;
+    private Map<Integer, Audio> cachedWindow = new HashMap<>();
 
     private final ArrayList<String> fullBook;
 
     private String audioCachDirPath = "/cach";
     private String bookName;
+    private String voice;
 
-    public void prefetchAndCleanUP() {
-        File directory = new File(audioCachDirPath);
+    public playState(int currentChunk, ArrayList<String> fullBook, int loadedRange, int reloadRange, int cacheSize, String bookName, TTSmodel initalModel, String voice) {
+        this.loadedRange = loadedRange;
+        this.reloadRange = reloadRange;
+        this.currentChunk = currentChunk;
+        this.cacheSize = cacheSize;
+        this.fullBook = fullBook;
+        startChunk = Math.max(0, (currentChunk - loadedRange));
+        endChunk = Math.min(fullBook.size() - 1, (currentChunk + loadedRange));
+        this.bookName = bookName;
+        this.initalModel = initalModel;
+        this.voice = voice;
+    }
 
-        File[] files = directory.listFiles();
+    public boolean isIncorectExtention(File file) {
+        int minFileExLength = 5;
+        String fileName = file.getName();
 
-        //TODO add error message.
-        if (files == null) {
-            return;
+        if (fileName.length() < minFileExLength) {
+            return true;
         }
 
-        ArrayList<File> filesToDelete = new ArrayList<>();
-        // TODO - Need to Add support for choosing the cash size
-        int cachSize = 5;
-
-        for (File file : files) {
-
-            // This checks if the file name is large enough to even be an .mp3 file
-            //5 is the minimum number of cars in a .mp4 file
-            String fullFileName = file.getName();
-            if (fullFileName.length() < 5) {
-                filesToDelete.add(file);
-                continue;
-            }
-
-            // Check for the correct extention
-            String extention = fullFileName.substring(fullFileName.length() - 4);
-            if (!extention.equals(".mp3")) {
-                filesToDelete.add(file);
-                continue;
-            }
-
-            // Gets the chunk number and the File name.\
-            // I chose to manually handel the cach becuase it works better when reloading
-            // The only reason the name is not just the chunk number, is so the if you exit the program and go back to the same bookafter you log back in, it will still have the cached chunks.
-            String fileName = fullFileName.substring(5, fullFileName.length() - 4);
-
-            String[] parts = fileName.split("_chunk_");
-            String audioFileBookName = parts[0];              // just the book name
-            int chunkNum = Integer.parseInt(parts[1]);  // just the chunk number
-
-            // Checks if the chunk is in the cach range.
-            if (chunkNum <= currentChunk - cachSize || chunkNum >= currentChunk + cachSize) {
-                filesToDelete.add(file);
-                cachedWindow.remove(chunkNum); // remove from memory too
-
-                continue;
-            }
-
-            // Checks if the chunk is in the correct book. This is mainly for when you first load the book
-            if (audioFileBookName != bookName) {
-                filesToDelete.add(file);
-                continue;
-            }
-
+        String extention = fileName.substring(fileName.length() - minFileExLength - 1);
+        if (!extention.equals(".mp3")) {
+            return true;
         }
 
-        // Removes all the files that need to be removed.
+        return false;
+    }
+
+    public boolean isIncorrectBook(File file) {
+
+        String fullFileName = file.getName();
+        String fileName = fullFileName.substring(5, fullFileName.length() - 4);
+
+        String[] parts = fileName.split("_chunk_");
+        String audioFileBookName = parts[0];              // just the book name
+
+        if (audioFileBookName != bookName) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean isOutOfCacheWindow(File file) {
+        String fullFileName = file.getName();
+        String fileName = fullFileName.substring(5, fullFileName.length() - 4);
+
+        String[] parts = fileName.split("_chunk_");
+
+        int chunkNum = Integer.parseInt(parts[1]);
+        if (chunkNum <= currentChunk - cacheSize || chunkNum >= currentChunk + cacheSize) {
+            
+            // It would be really annoing to add this anyware else
+            cachedWindow.remove(chunkNum);
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    public void deleteFiles(ArrayList<File> filesToDelete) {
+
         for (File file : filesToDelete) {
             if (file.delete()) {
 
@@ -106,40 +110,66 @@ public class playState {
                 System.out.println("Failed to delete the file");
             }
         }
+    }
 
-        //TODO: need to add the prefetching logic.
-        for (int i = currentChunk; i <= currentChunk + cachSize; i++) {
+    public void updateCachWindow() {
+        for (int i = currentChunk; i <= currentChunk + cacheSize; i++) {
             if (cachedWindow.containsKey(i)) {
                 continue;
             }
 
-            //TODO: add audio settings
-            //cachedWindow.put(i, new Audio());
+            int chunkInt = i;
+            cachedWindow.put(i, new Audio(fullBook.get(chunkInt), voice, initalModel.URL, initalModel.name, chunkInt, bookName));
         }
 
     }
 
-    public playState(int currentChunk, ArrayList<String> fullBook, int loadedRange, int reloadRange, String bookName) {
-        this.loadedRange = loadedRange;
-        this.reloadRange = reloadRange;
-        this.currentChunk = currentChunk;
-        this.fullBook = fullBook;
-        startChunk = Math.max(0, (currentChunk - loadedRange));
-        endChunk = Math.min(fullBook.size() - 1, (currentChunk + loadedRange));
-        this.bookName = bookName;
+    public void prefetchAndCleanUP() {
+        File directory = new File(audioCachDirPath);
+        File[] files = directory.listFiles();
 
-    }
+        //TODO add error message.
+        if (files == null) {
+            return;
+        }
 
-    public void getAudio() {
+        ArrayList<File> filesToDelete = new ArrayList<>();
+
+        // Checks if the files need to be deleted
+        for (File file : files) {
+
+            if (isIncorectExtention(file)) {
+                filesToDelete.add(file);
+                continue;
+            }
+
+            if (isIncorrectBook(file)) {
+                filesToDelete.add(file);
+                continue;
+            }
+
+            if (isOutOfCacheWindow(file)) {
+                filesToDelete.add(file);
+                continue;
+            }
+
+            if (isOutOfCacheWindow(file)) {
+                filesToDelete.add(file);
+                continue;
+            }
+
+        }
+        deleteFiles(filesToDelete);
+
+        updateCachWindow();
 
     }
 
     public void setCurrentChunk(int currentChunk) {
-
+        
+        //TODO: need to update the current chunk in AudioBookDB
         System.out.println("This is the full book size " + fullBook.size());
         this.currentChunk = currentChunk;
-        // I need to find some way to reset the the view.
-
     }
 
     public int getCurrentChunk() {
